ea4d8fa827c1d4c9726276aaa99286611918429d
Manish Bhandari <bhmanish73@gmail.com>
2022-03-13 14:24:37 -0500

    updated socials

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -4,9 +4,21 @@ import './Socials.css'
 function Socials() {
   return (
     <div className="socials-container">
-      <i class="fa-brands fa-linkedin"></i>
-      <i class="fa-brands fa-instagram"></i>
-      <i class="fa-brands fa-github"></i>
+      <a
+        class="fa-brands fa-linkedin"
+        target="_blank"
+        href="https://www.linkedin.com/in/bhandari-manish/"
+      ></a>
+      <a
+        class="fa-brands fa-instagram"
+        target="_blank"
+        href="https://www.instagram.com/manish.bhandariii/"
+      ></a>
+      <a
+        class="fa-brands fa-github"
+        href="https://github.com/manish-bhandari"
+        target="_blank"
+      ></a>
     </div>
   );
 }
