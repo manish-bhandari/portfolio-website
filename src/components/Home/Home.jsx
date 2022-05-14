37e4f46800fc4f1ad692fc181e8d1fcb9bc36ba0
Manish Bhandari <bhmanish73@gmail.com>
2022-05-14 03:30:17 -0500

    progress with home css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 import { Typewriter } from "react-simple-typewriter";
 
 function Home() {
   return (
-      <section className="home-section">
-        <div className="home-left">
+    <section className="home section">
+      <div className="home_content grid">
+        <div className="home_data">
           <h1>HI THERE 👋 MY NAME IS</h1>
           <h2>MANISH BHANDARI</h2>
           <p>
             I'm a Computer Science student at the University of Texas at Austin
             with a passion for{" "}
-            <Typewriter
-              words={[
-                "web development",
-                "full-stack",
-                "design",
-                "film making",
-                "dance",
-              ]}
-              loop={0}
-              cursor
-              cursorStyle="|"
-              typeSpeed={120}
-              deleteSpeed={80}
-              delaySpeed={1000}
-            />
+            {/* <Typewriter
+                words={[
+                  "web development",
+                  "full-stack",
+                  "design",
+                  "film making",
+                  "dance",
+                ]}
+                loop={0}
+                cursor
+                cursorStyle="|"
+                typeSpeed={120}
+                deleteSpeed={80}
+                delaySpeed={1000}
+              /> */}
           </p>
         </div>
-        <div className="home-right">
+        <div className="home_img">
           <img src="assets/guy-graphic.png" alt="" />
         </div>
-      </section>
+      </div>
+      <div className="home_scroll">
+        <h1>scroll down</h1>
+      </div>
+    </section>
   );
 }
 
-export default Home
+export default Home;
