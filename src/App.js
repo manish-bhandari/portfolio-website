de8b5aa1de6e62a8221ad9f72b473ba140f365aa
Manish Bhandari <bhmanish73@gmail.com>
2022-06-03 19:28:35 -0500

    added stars background

--- src/App.js
+++ src/App.js
@@ -1,66 +1,62 @@
-import './App.css';
-import {useEffect, useState} from 'react'
-import About from './components/About/About';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-import Socials from './components/Socials/Socials';
-import Footer from './components/Footer/Footer';
-import Skills from './components/Skills/Skills';
-import Experience from './components/Experience/Experience';
-import Work from './components/Work/Work';
+import "./App.css";
+import { useEffect, useState } from "react";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Socials from "./components/Socials/Socials";
+import Footer from "./components/Footer/Footer";
+import Skills from "./components/Skills/Skills";
+import Experience from "./components/Experience/Experience";
+import Work from "./components/Work/Work";
 // import OtherWork from './components/OtherWork/OtherWork';
-import ContactMe from './components/Contact Me/ContactMe';
+import ContactMe from "./components/Contact Me/ContactMe";
 import { BrowserRouter as Router } from "react-router-dom";
 import Aos from "aos";
-
+import Stars from "./components/Stars/Stars";
 
 function App() {
   useEffect(() => {
-    Aos.init({ disable:'mobile',duration: 1500, once: true});
+    Aos.init({ disable: "mobile", duration: 1500, once: true });
   }, []);
 
-  function getTheme(){
+  function getTheme() {
     let currTheme = localStorage.getItem("theme");
 
     if (currTheme) {
       return currTheme;
-    } else 
-      return 'dark'
+    } else return "dark";
   }
-  
 
   const [theme, setTheme] = useState(getTheme());
 
   const toggleTheme = () => {
-    let newTheme = theme === "light" ? "dark" : 'light';
-    localStorage.setItem("theme",newTheme);
+    let newTheme = theme === "light" ? "dark" : "light";
+    localStorage.setItem("theme", newTheme);
     setTheme(newTheme);
-  }
+  };
 
-
-  
-
-  useEffect(()=>{
+  useEffect(() => {
     console.log(theme);
-  },[theme])
+  }, [theme]);
 
   return (
-      <Router>
-        <div className="app" id="App" data-theme={theme}>
-            <Navbar theme={theme} toggleTheme={toggleTheme}/>
-            <div className="content">
-              <Home />
-              <About />
-              <Skills />
-              <Experience />
-              <Work />
-              {/* <OtherWork /> */}
-              <ContactMe />
-            </div>
-            <Socials />
-            <Footer />
-          </div>
-      </Router>
+    <Router>
+      <div className="app" id="App" data-theme={theme}>
+        <Stars />
+        <Navbar theme={theme} toggleTheme={toggleTheme} />
+        <div className="content">
+          <Home />
+          <About />
+          <Skills />
+          <Experience />
+          <Work />
+          {/* <OtherWork /> */}
+          <ContactMe />
+        </div>
+        <Socials />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Stars/Stars.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Stars.css";
+
+export default function Stars() {
+  return (
+    <div className="stars_container">
+      <div className="stars"></div>
+      <div className="stars1"></div>
+      <div className="stars2"></div>
+      <div className="shooting-stars"></div>
+    </div>
+  );
+}
